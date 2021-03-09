9263732ea2c7d04864c71984191132a2f7bd31a9
nicolatesla-max <78189979+nicolatesla-max@users.noreply.github.com>
2021-03-09 09:49:47 +0530

    First Commit

---
+++ src/main/java/com/ravindra/SpringBoootHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.ravindra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoootHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoootHelloWorldApplication.class, args);
+	}
+
+}
